54477ce7ea46607fc5e6e064ba0168b585b98c6e
alsahli38 <adilalshli242@gmail.com>
2025-10-01 22:26:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-// هنا يمكن إضافة أي أكواد JavaScript مستقبلية
-// مثال: تفعيل إعلان AdSense
-(window.adsbygoogle = window.adsbygoogle || []).push({});
+atOptions = {
+  'key' : 'd065b8f6e363447d9106698f007a3bc7',
+  'format' : 'iframe',
+  'height' : 300,
+  'width' : 160,
+  'params' : {}
+};
+document.write('<script type="text/javascript" src="//www.highperformanceformat.com/d065b8f6e363447d9106698f007a3bc7/invoke.js"><\/script>');
